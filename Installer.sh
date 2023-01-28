0819eed0151a223203d56ab46d3bb07cf7978a88
jeanlazo <69772250+jeanlazo@users.noreply.github.com>
2023-01-28 11:06:24 -0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -4,20 +4,20 @@ apt install -y xz-utils openssl gawk file wget figlet
 clear
 figlet -c JeanTEC
 figlet -c CARIZA.03
-sleep 3
+sleep 2
 clear
 echo "Original Autor:"
 figlet -c JOHNY BY CARLOS
 sleep 2
 clear
 echo -e "\x1b[1;31m Este script solo está probado con Oracle Cloud, la instalacion dura de 1 hora a 1 hora con 30 minutos aprox... Al instalarse cambie la contraseña de administrador para mayor seguridad\x1b"
-sleep 10
+sleep 4
 echo "Usuario Administrador:"
 figlet -c Administrator
-sleep 3
+sleep 2
 echo Contra de Administrador:
 figlet -c "nat.ee"
-sleep 4
+sleep 2
 clear
 figlet -c Iniciando
 sleep 2
